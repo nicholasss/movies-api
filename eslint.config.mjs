c25f6f7fa61821cf105a3d124425bea0f3e36781
Nicholas <nicholasss@users.noreply.github.com>
2025-11-27 11:44:06 -0500

    feat: switch to bun and updated eslint.config.mjs to use module intead of commonjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -4,7 +4,7 @@ import json from "@eslint/json";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
-  { files: ["**/*.js"], languageOptions: { sourceType: "commonjs" } },
-  { files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
+	{ files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+	{ files: ["**/*.js"], languageOptions: { sourceType: "module" } },
+	{ files: ["**/*.json"], plugins: { json }, language: "json/json", extends: ["json/recommended"] },
 ]);
