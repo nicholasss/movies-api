8cfbf4be1c17c8032dff55c919e8b0e9ddfc7c1a
Nicholas <nicholasss@users.noreply.github.com>
2026-03-05 22:09:17 -0500

    updated eslint conifg

--- eslint.config.js
+++ eslint.config.js
@@ -3,5 +3,16 @@ import globals from "globals";
 import { defineConfig } from "eslint/config";
 
 export default defineConfig([
-  { files: ["**/*.{js,mjs,cjs}"], plugins: { js }, extends: ["js/recommended"], languageOptions: { globals: globals.node } },
+	// Spread the recommended configuration first
+	js.configs.recommended,
+
+	{
+		files: ["**/*.{js,mjs,cjs}"],
+		languageOptions: {
+			globals: globals.node,
+		},
+		rules: {
+			"no-unused-vars": "warn",
+		},
+	},
 ]);
